ea97be3148a5c1d30b230168d51c989eb487fd19
Lepidophylla <ericroberto2322@gmail.com>
2022-09-13 08:36:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_11007/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_11007
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Pergi Tidur", "Kebiasaan sehari-hari untuk membuat hari makin sedih"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Kerjaan mahasiswa nolep"),
+        Task("Chapter 3", "Ngopi", "Habis capek, terbitlah tugas"),
+        Task("Chapter 4", "Kenapa ini susah?", "Makanya belajar"),
+        Task("Chapter 5", "Upload ke Instagram", "Cara pamer tanpa kelihatan pamer")
+    )
+}
